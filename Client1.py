b3b65f4c80d04c03493e614e8cf655e64b3026cf
Dylan Shade <dpshadey22@gmail.com>
2022-11-12 15:56:14 -0500

    Boiler Plate

--- Client1.py
+++ Client1.py
@@ -1,1 +1,13 @@
 # this is client 1 for the Fall 2022 CS 371 Project
+
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
