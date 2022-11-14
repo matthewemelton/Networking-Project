ecd6889c7d673742a0aa70ce7e25259374c642c5
Dylan Shade <63427984+dpshade22@users.noreply.github.com>
2022-11-14 15:48:59 -0500

    init functions on File Obj

--- FileClass.py
+++ FileClass.py
@@ -1,12 +1,27 @@
 import os
 
-class File:
-  def __init__(self, name):
-    self.name = name
-    self.path = f"./{name}"
-    self.downloads = 0
-    self.fileContents = None
-    self.fileBytes = None
+FORMAT = "utf-8"
 
-  def initSize(self):
-    self.fileSize = os.path.getsize(f"./ServerFiles/{self.name}")
+
+class File:
+    def __init__(self, name):
+        self.name = name
+        self.path = f"./{name}"
+        self.downloads = 0
+        self.fileContents = None
+        self.fileBytes = None
+        self.fileSize = -1
+
+    def addFileToServer(self):
+        with open(f"./ServerFiles/{self.name}", "w") as f:
+            f.write(self.fileContents)
+
+        self.fileSize = os.path.getsize(f"./ServerFiles/{self.name}")
+
+    def initContentGivenBytes(self, bytes: bytes):
+        self.fileBytes = bytes
+        self.fileContents = bytes.decode(FORMAT)
+
+    def initBytesGivenContent(self, content: str):
+        self.fileContents = content
+        self.fileBytes = content.encode(FORMAT)
--- Server.py
+++ Server.py
@@ -2,9 +2,10 @@
 
 import socket, threading, _thread as thread, os
 from FileClass import File
+
 print_lock = threading.Lock()
 
-FORMAT = 'utf-8'
+FORMAT = "utf-8"
 
 numClients = 0
 numThreads = 0
@@ -12,158 +13,152 @@ clients = []
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if not os.path.exists("./ServerFiles"):
-  os.mkdir("./ServerFiles")
+    os.mkdir("./ServerFiles")
 
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 55321  # Port to listen on (non-privileged ports are > 1023)
 fileStorage = {}
 
+
 def Upload(fileName, connection):
-  fileBytes = connection.recv(1024)
-  
-  fileStorage[fileName] = File(fileName)
-  fileObj = fileStorage[fileName]
-  fileObj.fileBytes = fileBytes
-  fileObj.fileContents = fileBytes.decode(FORMAT)
+    fileBytes = connection.recv(1024)
 
-  with open(f"./ServerFiles/{fileName}", "w") as f:
-    f.write(fileObj.fileContents)
+    fileStorage[fileName] = File(fileName)
+    fileObj = fileStorage[fileName]
 
-  fileObj.initSize()
+    fileObj.initContentGivenBytes(fileBytes)
+    fileObj.addFileToServer()
 
-  
-  print(f"""
+    print(
+        f"""
   {fileObj.name}
   {fileObj.path}
   {fileObj.fileSize}
   {fileObj.downloads}
   {fileObj.fileContents}
-  """)
-  
+  """
+    )
+
+
 def Download(fileName, connection):
-  existsOnServer, fileObj = GetFile(fileName)
-  fileObj = fileStorage[fileName]
+    existsOnServer, fileObj = GetFile(fileName)
+    fileObj = fileStorage[fileName]
 
-  connection.sendall(fileObj.fileBytes)
-  
-  fileObj.initSize()
-  fileObj.downloads += 1
-  
-  print(f"""
+    connection.sendall(fileObj.fileBytes)
+
+    fileObj.downloads += 1
+
+    print(
+        f"""
   {fileObj.name}
   {fileObj.path}
   {fileObj.fileSize}
   {fileObj.downloads}
   {fileObj.fileContents}
-  """)
+  """
+    )
 
-  
-  ackData = connection.recv(1024)
-  ackData = ackData.decode(FORMAT)
-  if ackData == "ACK":
-    if not existsOnServer:
-      if os.path.isfile(fileName):
-        os.remove(fileName)
-      else:
-        print("ERROR: file does not exist\n")
-  else:
-    print("ERROR: no ack from client\n")
-    
-  
+    ackData = connection.recv(1024)
+    ackData = ackData.decode(FORMAT)
+    if ackData == "ACK":
+        if not existsOnServer:
+            if os.path.isfile(fileName):
+                os.remove(fileName)
+            else:
+                print("ERROR: file does not exist\n")
+    else:
+        print("ERROR: no ack from client\n")
+
+
 def HandleClient(connection):
-  # initalize the client and give the client an ID
-  global numThreads
-  clientID = str(numClients)
-  print(f"I am handling a client in thread {numThreads}")
+    # initalize the client and give the client an ID
+    global numThreads
+    clientID = str(numClients)
+    print(f"I am handling a client in thread {numThreads}")
 
-  while True:
-    # wait for the client to send a command
-    data = connection.recv(1024)
-    data = data.decode(FORMAT)
-    
-    command, fileName = data.split()
-    
-    if command == "UPLOAD":
-      Upload(fileName, connection)
+    while True:
+        # wait for the client to send a command
+        data = connection.recv(1024)
+        data = data.decode(FORMAT)
 
-    
-    elif command == "DOWNLOAD":
-      Download(fileName, connection)
-      
-      
-    elif data.decode(FORMAT) == "DISCONNECT":
-      temp = "GoodBye"
-      connection.send(temp.encode(FORMAT))
-      connection.close()
-      break
-  
+        command, fileName = data.split()
+
+        if command == "UPLOAD":
+            Upload(fileName, connection)
+
+        elif command == "DOWNLOAD":
+            Download(fileName, connection)
+
+        elif data.decode(FORMAT) == "DISCONNECT":
+            temp = "GoodBye"
+            connection.send(temp.encode(FORMAT))
+            connection.close()
+            break
 
 
 def GetFile(fileName):
-  existsOnServer = True
+    existsOnServer = True
 
-  fileStorage[fileName] = File(fileName)
-  fileObj = fileStorage[fileName]
-  
-  if not os.path.exists(f"./ServerFiles/{fileName}"):
-    existsOnServer = False
-    # we need to check if the other client has it... 
-    # make fileObj.fileContents = to client 2's file
-    # fileObj.size = size of client 2's file
-    
-    fileObj.fileContents = "BYTES COLLECTED FROM CLIENT 2"
-  else:
-    with open(f"./ServerFiles/{fileName}", "r") as f:
-      data = f.read()
-      fileObj.fileContent = data
-      fileObj.fileBytes = data.encode(FORMAT)
-    
-    # the server has the file
+    fileStorage[fileName] = File(fileName)
+    fileObj = fileStorage[fileName]
 
-  return (existsOnServer, fileObj)
-  
-  
+    if not os.path.exists(f"./ServerFiles/{fileName}"):
+        existsOnServer = False
+        # we need to check if the other client has it...
+        # make fileObj.fileContents = to client 2's file
+        # fileObj.size = size of client 2's file
+
+        fileObj.fileContents = "BYTES COLLECTED FROM CLIENT 2"
+    else:
+        with open(f"./ServerFiles/{fileName}", "r") as f:
+            data = f.read()
+            fileObj.initBytesGivenContent(data)
+
+        # the server has the file
+
+    return (existsOnServer, fileObj)
+
+
 def Main():
-  # bind the socket to the correct host and port
-  # prints an error if the socket cannot be bound
-  try:
-    s.bind((HOST, PORT))
+    # bind the socket to the correct host and port
+    # prints an error if the socket cannot be bound
+    try:
+        s.bind((HOST, PORT))
 
-  except s.error() as socketError:
-    print(str(socketError))
+    except s.error() as socketError:
+        print(str(socketError))
 
-  while True:
-    s.listen(5)
-    print("Socket is listening")
+    while True:
+        s.listen(5)
+        print("Socket is listening")
 
-    # this does work
-    conn, addr = s.accept()
-    clients.append((conn, addr))
-    global numClients
-    numClients += 1
-    
-    client1Addr = clients[0][1][0]
-    client1Port = clients[0][1][1]
-    client2Addr = None
-    client2Port = None
-    
-    if numClients == 2:
-      client2Addr = clients[1][1][0]
-      client2Port = clients[1][1][1]
+        # this does work
+        conn, addr = s.accept()
+        clients.append((conn, addr))
+        global numClients
+        numClients += 1
 
-    
-    # print(f"Connected to {addr[0]}:{addr[1]}")
-    print(f"CLIENT1 = Address: {client1Addr}\n Port: {client1Port}\n")
-    print(f"CLIENT2 = Address: {client2Addr}\n Port: {client2Port}\n")
+        client1Addr = clients[0][1][0]
+        client1Port = clients[0][1][1]
+        client2Addr = None
+        client2Port = None
 
-    thread.start_new_thread(HandleClient, (clients[0][0], ))
-    global numThreads
-    numThreads += 1
+        if numClients == 2:
+            client2Addr = clients[1][1][0]
+            client2Port = clients[1][1][1]
 
-  # server should never reach this line of code unless we break from the above loop
-  s.close()
+        # print(f"Connected to {addr[0]}:{addr[1]}")
+        print(f"CLIENT1 = Address: {client1Addr}\n Port: {client1Port}\n")
+        print(f"CLIENT2 = Address: {client2Addr}\n Port: {client2Port}\n")
+
+        thread.start_new_thread(HandleClient, (clients[0][0],))
+        global numThreads
+        numThreads += 1
+
+    # server should never reach this line of code unless we break from the above loop
+    s.close()
 
 
-if __name__ == '__main__':
-  Main()
+if __name__ == "__main__":
+    Main()
